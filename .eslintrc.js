b79b3fe9d46f08325c093afe7b8fe5e11db2a58e
anasyoussef00 <anasyoussef.elmahdad00@proton.me>
2023-09-29 16:19:28 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+};
