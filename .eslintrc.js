22dbe0829252ddf32142a227f7f9245b4a469b66
anasyoussef00 <anasyoussef.elmahdad00@proton.me>
2023-09-30 01:41:47 +0100

    changing the repo to a mono-repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,16 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/stylistic',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/ban-types': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+  },
 };
